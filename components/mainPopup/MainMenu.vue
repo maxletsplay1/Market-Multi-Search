cb2f15017fdc04e165d34a3c57d50ebc04c22799
maksik <123letsplay93@gmail.com>
2025-09-02 21:56:48 +0700

    FEAT: added footer for popup

--- components/mainPopup/MainMenu.vue
+++ components/mainPopup/MainMenu.vue
@@ -27,6 +27,15 @@
           :query="query"
       />
     </div>
+    <hr>
+    <a
+        href="https://github.com/maxletsplay1/Market-Multi-Search"
+        target="_blank"
+        class="flex items-center justify-center gap-1 -mt-1"
+    >
+      <i class="pi pi-github"/>
+      <p class="text-sm">GitHub</p>
+    </a>
   </div>
 </template>
 
